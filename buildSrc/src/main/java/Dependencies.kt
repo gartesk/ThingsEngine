e1c1ccf56a0755d772297f06e78ffeb4ae70fe6b
Kirill Garbuzov <kirill.e.garbuzov@gmail.com>
2018-06-03 15:27:51 +0700

    Added empty modules

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,25 @@
+object Versions {
+    const val supportLib = "27.1.1"
+    const val rxJava = "2.1.12"
+    const val rxAndroid = "2.0.2"
+    const val things = "1.0"
+    const val playServices = "11.6.0"
+    const val kotlin = "1.2.41"
+    const val gradle = "3.1.0"
+    const val targetSdk = 27
+    const val compileSdk = 27
+}
+
+object Libs {
+    const val supportAppcompatV7 = "com.android.support:appcompat-v7:${Versions.supportLib}"
+    const val rxJava = "io.reactivex.rxjava2:rxjava:${Versions.rxJava}"
+    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
+    const val things = "com.google.android.things:androidthings:${Versions.things}"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jre7:${Versions.kotlin}"
+    const val playServicesNearby = "com.google.android.gms:play-services-nearby:${Versions.playServices}"
+}
+
+object Plugins {
+    const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val gradle = "com.android.tools.build:gradle:${Versions.gradle}"
+}
---
+++ server/src/main/java/com/gartesk/thingsengine/server/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gartesk.thingsengine.server
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
